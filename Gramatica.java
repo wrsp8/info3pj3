6747cb844536f548eb52f91615ca816a9184aec9
wrsp8 <wrsp@hotmail.com>
2020-05-06 23:53:51 -0600

    AFD con fallos

--- Gramatica.java
+++ Gramatica.java
@@ -37,12 +37,36 @@ public class Gramatica{
 			System.exit(0);
 			ropes = args[3];
 			//metodo para evaluar cuerdas
+			AFN afn = new AFN(GLDtoAFN(states, alphabet, initState, transitions));
+			Scanner cuerdas = new Scanner("");
+			try {
+				cuerdas = new Scanner(new File(ropes));
+			}catch(Exception e){}
+			
+			FileWriter file = new FileWriter(saveFile);;
+			try
+			{
+				while(cuerdas.hasNextLine()){
+					if(afn.accept(cuerdas.nextLine())){
+						file.write("aceptada");
+					} else {
+						file.write("rechazada");
+					}
+				}	
+			} catch (Exception e){}
+			file.close();
+			cuerdas.close();
 		} else {
 			if(executionMode.equals("-afd")) {
 				//metodo para generar el afd
+				String answer =GLDtoAFN(states, alphabet, initState,transitions);
+				answer = AFNtoAFD(answer,alphabet);
+				AFNFile(answer, saveFile);
+
 			} else {
 				//metodo para generar el afn
-				GLDtoAFN(states, alphabet, initState,transitions);
+				String answer =GLDtoAFN(states, alphabet, initState,transitions);
+				AFNFile(answer, saveFile);
 				System.exit(0);
 			}
 		}
@@ -55,22 +79,10 @@ public class Gramatica{
 
 		for(String s: transitions){
 			int len = s.length();
-			if(len>=5){
+			if(len>=5&&findPos(symbols,s.charAt(len-1))==-1){
+				states += len - 4;
+			} else if (len>5&&findPos(symbols,s.charAt(len-1))!=-1){
 				states += len - 5;
-				if(findPos(symbols,s.charAt(len-1))==-1){
-					states++;	
-				}
-			}
-		}
-		
-		
-		for(String s: transitions){
-			int len = s.length();
-			if(len>=5){
-				states += len - 5;
-				if(findPos(symbols,s.charAt(len-1))==-1){
-					states++;	
-				}
 			}
 		}
 		String[][] mat = new String[endSymbols.length+1][states];
@@ -138,10 +150,12 @@ public class Gramatica{
 			for(int j = 0; j< mat[i].length-1; j++){
 				resp += mat[i][j] + ",";
 			}
-			resp += mat[i][mat.length-1];
+			resp += mat[i][mat[i].length-1];
 			resp+="\n";
 		}
-		System.out.println(resp);
+		//System.out.println(resp);
+
+		//System.out.println(Arrays.deepToString(mat));
 		return resp;
 	}	
 
@@ -152,6 +166,112 @@ public class Gramatica{
 				return i;
 			}
 		}
+		return -1;
+	}
+
+	public static void AFNFile(String afn, String saveFile){
+		
+		try{
+			File file = new File(saveFile);
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(afn);
+			bw.close();
+		} catch (Exception e){
+
+		}
+	
+	}
+
+	public static String AFNtoAFD(String afn, String[] alphabet){
+		AFN objAFN = new AFN(afn);
+		LinkedList<int[]> states = new LinkedList<int[]>();
+		int[] first = {1};
+		first = removeDuplicates(objAFN.getTransition(first,'#'));
+		System.out.println(Arrays.toString(first));
+		states.add(first);
+		LinkedList<int[]> transitions = new LinkedList<int[]>();
+		LinkedList<int[]> toVisit = new LinkedList<int[]>();
+		toVisit.add(first);
+		while(!toVisit.isEmpty()){
+			int[] thisTransitions = new int[alphabet.length];
+			int pos = 0;
+			for(String s: alphabet){
+				int[] result = removeDuplicates(objAFN.getTransition(toVisit.getFirst(),s.charAt(0)));
+				result = removeDuplicates(objAFN.getTransition(result,'#'));
+				int exists = findElement(states,result);
+				if(exists == -1){
+					toVisit.addLast(result);
+					states.addLast(result);
+					exists = states.size();
+				}
+				thisTransitions[pos] = exists;
+				pos++;
+			}
+			transitions.addLast(thisTransitions);
+			toVisit.removeFirst();
+		}
+
+		String resp = "";
+		for(int i = 0; i< alphabet.length-1;i++){
+			resp+=alphabet[i]+",";
+		}
+		resp+=alphabet[alphabet.length-1];
+		resp+="\n";
+		resp+=Integer.toString(transitions.size());
+		resp+="\n";
+		int AFNFinalState = objAFN.finalStates[0];
+		LinkedList<Integer> finalAFD = new LinkedList<Integer>();
+		int pos = 0;
+		for(int[] i : states){
+			for(int j = 0; j < i.length; j++){
+				if(AFNFinalState == i[j]){
+					finalAFD.add(pos);
+					break;
+				}
+			}
+			pos++;
+		}
+
+		int[] arrayFinal = finalAFD.stream().mapToInt(i->i).toArray();;
+		for(int i = 0; i < arrayFinal.length -1; i++){
+			resp+=Integer.toString(arrayFinal[i])+",";
+		}
+		resp+=Integer.toString(arrayFinal.length -1);
+		resp+="\n";
+		for(int j = 0; j < alphabet.length; j++){
+			for(int i = 0 ; i < transitions.size() -1; i++){
+				resp+=Integer.toString(transitions.get(i)[j]);
+			}
+			resp += Integer.toString(transitions.get(transitions.size()-1)[j]);
+			resp+="\n";
+		}
+		
+		//System.out.println(resp);
+		return resp;
+
+	}
+
+	public static int[] removeDuplicates(int[] arr){
+		return Arrays.stream(arr).distinct().toArray();
+	}
+
+	public static int findElement(LinkedList<int[]> list,int[] object){
+		int pos = 0;
+		for(int[] i : list){
+			if(i.length==object.length){
+				for(int j = 0; j < object.length; j++){
+					if(i[j]!=object[j])
+						break;
+				}
+				return pos;
+			}
+			pos++;
+		}
+
 		return -1;
 	}
 
